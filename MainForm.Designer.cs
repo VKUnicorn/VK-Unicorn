d1487ee983f4873deca7c12e383366d0034507da
VKUnicorn <>
2019-11-01 21:22:04 +0700

    Начальный коммит

---
+++ MainForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace VK_Unicorn
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.LogTextBox = new System.Windows.Forms.RichTextBox();
+            this.ShowResults = new System.Windows.Forms.Button();
+            this.LogGroupBox = new System.Windows.Forms.GroupBox();
+            this.VersionLabel = new System.Windows.Forms.Label();
+            this.StatisticsLabel = new System.Windows.Forms.Label();
+            this.LogGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitButton.Location = new System.Drawing.Point(548, 331);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(118, 38);
+            this.ExitButton.TabIndex = 0;
+            this.ExitButton.TabStop = false;
+            this.ExitButton.Text = "Выйти";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // LogTextBox
+            // 
+            this.LogTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.LogTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LogTextBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LogTextBox.Location = new System.Drawing.Point(3, 16);
+            this.LogTextBox.Name = "LogTextBox";
+            this.LogTextBox.ReadOnly = true;
+            this.LogTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.LogTextBox.Size = new System.Drawing.Size(661, 309);
+            this.LogTextBox.TabIndex = 1;
+            this.LogTextBox.TabStop = false;
+            this.LogTextBox.Text = "Проверка текста\nTest jumps over lazy dog";
+            // 
+            // ShowResults
+            // 
+            this.ShowResults.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ShowResults.Location = new System.Drawing.Point(425, 331);
+            this.ShowResults.Name = "ShowResults";
+            this.ShowResults.Size = new System.Drawing.Size(118, 38);
+            this.ShowResults.TabIndex = 2;
+            this.ShowResults.TabStop = false;
+            this.ShowResults.Text = "Результаты";
+            this.ShowResults.UseVisualStyleBackColor = true;
+            // 
+            // LogGroupBox
+            // 
+            this.LogGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LogGroupBox.Controls.Add(this.LogTextBox);
+            this.LogGroupBox.Location = new System.Drawing.Point(0, 0);
+            this.LogGroupBox.Name = "LogGroupBox";
+            this.LogGroupBox.Size = new System.Drawing.Size(667, 328);
+            this.LogGroupBox.TabIndex = 3;
+            this.LogGroupBox.TabStop = false;
+            this.LogGroupBox.Text = "Лог";
+            // 
+            // VersionLabel
+            // 
+            this.VersionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.VersionLabel.AutoSize = true;
+            this.VersionLabel.Enabled = false;
+            this.VersionLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.VersionLabel.Location = new System.Drawing.Point(1, 351);
+            this.VersionLabel.Name = "VersionLabel";
+            this.VersionLabel.Size = new System.Drawing.Size(40, 16);
+            this.VersionLabel.TabIndex = 4;
+            this.VersionLabel.Text = "v1.0";
+            // 
+            // StatisticsLabel
+            // 
+            this.StatisticsLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.StatisticsLabel.AutoSize = true;
+            this.StatisticsLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StatisticsLabel.ForeColor = System.Drawing.Color.Green;
+            this.StatisticsLabel.Location = new System.Drawing.Point(1, 332);
+            this.StatisticsLabel.Name = "StatisticsLabel";
+            this.StatisticsLabel.Size = new System.Drawing.Size(88, 16);
+            this.StatisticsLabel.TabIndex = 5;
+            this.StatisticsLabel.Text = "Статистика";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 371);
+            this.Controls.Add(this.StatisticsLabel);
+            this.Controls.Add(this.VersionLabel);
+            this.Controls.Add(this.LogGroupBox);
+            this.Controls.Add(this.ShowResults);
+            this.Controls.Add(this.ExitButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VK Unicorn";
+            this.LogGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.RichTextBox LogTextBox;
+        private System.Windows.Forms.Button ShowResults;
+        private System.Windows.Forms.GroupBox LogGroupBox;
+        private System.Windows.Forms.Label VersionLabel;
+        private System.Windows.Forms.Label StatisticsLabel;
+    }
+}
+
